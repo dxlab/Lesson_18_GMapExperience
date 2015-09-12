a287c7887c67e5265e9361186d51f0b21656c637
dxlab <ds@dxlab.com.ua>
2015-09-12 18:48:49 +0300

    CustomLocationListener class created

---
+++ app/src/main/java/ua/com/dxlab/lesson_18_gmapexperience/CustomLocationListener.java
@@ -0,0 +1,36 @@
+package ua.com.dxlab.lesson_18_gmapexperience;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by Dima on 12.09.2015.
+ */
+public class CustomLocationListener  implements LocationListener {
+
+    private MapsActivity mMapsActivity;
+
+    public CustomLocationListener(MapsActivity mapsActivity) {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
